d54984f7c41ba48f1ba9ab2851568bb5d2ff65f0
gobblygobble <gobblygobble@kaist.ac.kr>
2020-04-16 21:52:10 +0900

    prelab3 q5 Keyboard_Control_TMR.c complete

--- lab3/keyboard-control/Keyboard_Control_TMR.c
+++ lab3/keyboard-control/Keyboard_Control_TMR.c
@@ -10,7 +10,9 @@
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <math.h>
+#include "gpio_control.h"
 
 #define eof 4
 #define esc 27
@@ -33,9 +35,16 @@ int main(void) {
     int found_pwm1_dir = 0;
     int found_pwm2_dir = 0;
     int how_many_slots = 0;
+    int fd30, fd31;
+    unsigned int last_written_led_30 = 0u;
+    unsigned int last_written_led_31 = 0u;
     int fd_run_0, fd_period_0, fd_duty_0;
     int fd_run_1, fd_period_1, fd_duty_1;
     int fd_run_2, fd_period_2, fd_duty_2;
+    int duty_ns0, duty_ns1, duty_ns2;
+    char *duty_ns_str0 = calloc(8, sizeof(char));
+    char *duty_ns_str1 = calloc(8, sizeof(char));
+    char *duty_ns_str2 = calloc(8, sizeof(char));
     char *pwm0_dir_name = calloc(18, sizeof(char));
     char *pwm1_dir_name = calloc(18, sizeof(char));
     char *pwm2_dir_name = calloc(18, sizeof(char));
@@ -244,41 +253,41 @@ int main(void) {
     strncat(afp_pwm2_duty, "duty", 4);
     // ==================== init for PWM0 ====================
     // "echo 0 > run"
-    fd_run_0 = open(afp_pwm0_run);
+    fd_run_0 = open(afp_pwm0_run, O_RDWR);
     write(fd_run_0, "0", 1);
     close(fd_run_0);
     // "echo 3000000 > period"
-    fd_period_0 = open(afp_pwm0_period);
+    fd_period_0 = open(afp_pwm0_period, O_RDWR);
     write(fd_period_0, "3000000", 7);
     close(fd_period_0);
     // "echo 1 > run"
-    fd_run_0 = open(afp_pwm0_run);
+    fd_run_0 = open(afp_pwm0_run, O_RDWR);
     write(fd_run_0, "1", 1);
     close(fd_run_0);
     // ==================== init for PWM1 ====================
     // "echo 0 > run"
-    fd_run_1 = open(afp_pwm1_run);
+    fd_run_1 = open(afp_pwm1_run, O_RDWR);
     write(fd_run_1, "0", 1);
     close(fd_run_1);
     // "echo 3000000 > period"
-    fd_period_1 = open(afp_pwm1_period);
+    fd_period_1 = open(afp_pwm1_period, O_RDWR);
     write(fd_period_1, "3000000", 7);
     close(fd_period_1);
     // "echo 1 > run"
-    fd_run_1 = open(afp_pwm1_run);
+    fd_run_1 = open(afp_pwm1_run, O_RDWR);
     write(fd_run_1, "1", 1);
     close(fd_run_1);
     // ==================== init for PWM2 ====================
     // "echo 0 > run"
-    fd_run_2 = open(afp_pwm2_run);
+    fd_run_2 = open(afp_pwm2_run, O_RDWR);
     write(fd_run_2, "0", 1);
     close(fd_run_2);
     // "echo 3000000 > period"
-    fd_period_2 = open(afp_pwm2_period);
+    fd_period_2 = open(afp_pwm2_period, O_RDWR);
     write(fd_period_2, "3000000", 7);
     close(fd_period_2);
     // "echo 1 > run"
-    fd_run_2 = open(afp_pwm2_run);
+    fd_run_2 = open(afp_pwm2_run, O_RDWR);
     write(fd_run_2, "1", 1);
     close(fd_run_2);
     
@@ -292,28 +301,153 @@ int main(void) {
     fprintf(stdout, "+---------------------------------------------------------+\n");
     
     // 5. loop
+    // before we loop, get ready to use LED
+    gpio_export(30u);
+    gpio_export(31u);
+    gpio_set_dir(30u, 1u);
+    gpio_set_dir(31u, 1u);
+    fd30 = gpio_fd_open(30u);
+    fd31 = gpio_fd_open(31u);
     while (1) {
+loop_start:
         // A. key input without Enter
         char c = getch();
         // B. 'esc' or 'ctrl-D' key terminates
         if ((c == eof) || (c == esc))
             break;
         // C. vx, vy, and w [%] from key input: dec/inc speed by 10%. do saturation
-        ;
+        switch (c)
+        {
+        case 's':
+            ivx = 0;
+            ivy = 0;
+            iw = 0;
+            break;
+        case 'w':
+            ivx = (ivx == 100) ? ivx : (ivx + 10);
+            break;
+        case 'x':
+            ivx = (ivx == -100) ? ivx : (ivx - 10);
+            break;
+        case 'a':
+            ivy = (ivy == 100) ? ivy : (ivy + 10);
+            break;
+        case 'd':
+            ivy = (ivy == -100) ? ivy : (ivy - 10);
+            break;
+        case 'z':
+            iw = (iw == 100) ? iw : (iw + 10);
+        case 'c':
+            iw = (iw == -100) ? iw : (iw - 10);
+            break;
+        case 'q':
+            // left light -> (right light when upside down) GPIO 31
+            gpio_fd_set_value(fd31, (1u - last_written_led_31));
+            break;
+        case 'e':
+            // right light -> (left light when upside down) GPIO 30
+            gpio_fd_set_value(fd30, (1u - last_written_led_30));
+            break;
+        default:
+            goto loop_start;
+            break;
+        }
         // D. transform vx, vy, w [%] ->[m/s, rad/s] -> kinematics -> ww_rpm[] -> duty_ns
-        ;
+        // D - a: calculate vx, vy, wr
+        vx = gain_v * (double)ivx;
+        vy = gain_v * (double)ivy;
+        wr = gain_w * (double)iw;
+        // D - b: transform vector v [vx vy wr]' to vector w [w1 w2 w3]' with "w = Tv"
+        // the values of w1, w2, w3 are directly taken from 
+        w1 = (-1 * L * wr - vy) / r;
+        w2 = (-1 * L * wr + vy / 2 + sqrt(3) * vx / 2) / r;
+        w3 = (-1 * L * wr + vy / 2 - sqrt(3) * vx / 2) / r;
         // E. compensate deadband
-        ;
+        // radian w -> 30w/pi rpm -> apply to compensating deadband equation
+        // wheel 0
+        if (w1 > 0)
+            duty_ns0 = (int)((1.45 + 0.27 * w1 / pi) * 1000000);
+        else if (w1 < 0)
+            duty_ns0 = (int)((1.55 + 0.27 * w1 / pi) * 1000000);
+        else
+            duty_ns0 = 1500000;
+        // wheel 1
+        if (w2 > 0)
+            duty_ns1 = (int)((1.45 + 0.27 * w2 / pi) * 1000000);
+        else if (w2 < 0)
+            duty_ns1 = (int)((1.55 + 0.27 * w2 / pi) * 1000000);
+        else
+            duty_ns1 = 1500000;
+        // wheel 2
+        if (w3 > 0)
+            duty_ns2 = (int)((1.45 + 0.27 * w3 / pi) * 1000000);
+        else if (w3 < 0)
+            duty_ns2 = (int)((1.55 + 0.27 * w3 / pi) * 1000000);
+        else
+            duty_ns2 = 1500000;
         // F. output PWM via sysfs with duty_ns considering pos/neg deadband/gain,
         //    also accurate lights
-        ;
+        // get ready to write to duty files
+        fd_duty_0 = open(afp_pwm0_duty, O_RDWR);
+        fd_duty_1 = open(afp_pwm1_duty, O_RDWR);
+        fd_duty_2 = open(afp_pwm2_duty, O_RDWR);
+        // get duty_ns_str's
+        snprintf(duty_ns_str0, 7, "%d", duty_ns0);
+        snprintf(duty_ns_str1, 7, "%d", duty_ns1);
+        snprintf(duty_ns_str2, 7, "%d", duty_ns2);
+        // write
+        write(fd_duty_0, duty_ns_str0, strlen(duty_ns_str0));
+        write(fd_duty_1, duty_ns_str1, strlen(duty_ns_str1));
+        write(fd_duty_2, duty_ns_str2, strlen(duty_ns_str2));
+        // close to commit change
+        close(fd_duty_0);
+        close(fd_duty_1);
+        close(fd_duty_2);
         // G. print key, vx/vy/w, duty_ns
-        ;
+        fprintf(stdout, "vx: %g(m/s), vy: %g(m/s), wr: %g(rad/s)\n", vx, vy, wr);
+        fprintf(stdout, "duty0, 1, 2: %d, %d, %d(ns)\n", duty_ns0, duty_ns1, duty_ns2);
+        fprintf(stdout, "===========================================================\n");
         // H. usleep(1ms)
         usleep(1000);
-        ;
     }
-
+    // before exit, stop PWMs
+    fd_run_0 = open(afp_pwm0_run, O_RDWR);
+    fd_run_1 = open(afp_pwm1_run, O_RDWR);
+    fd_run_2 = open(afp_pwm2_run, O_RDWR);
+    write(fd_run_0, "0", 1);
+    write(fd_run_1, "0", 1);
+    write(fd_run_2, "0", 1);
+    close(fd_run_0);
+    close(fd_run_1);
+    close(fd_run_2);
+    // before exit, clean up LED usage
+    gpio_fd_close(fd30);
+    gpio_fd_close(fd31);
+    gpio_set_dir(30u, 0u);
+    gpio_set_dir(31u, 0u);
+    gpio_unexport(30u);
+    gpio_unexport(31u);
+    // before exit, free all allocated pointers
+    free(bone_capemgr);
+    free(abs_bone_capemgr);
+    free(duty_ns_str0);
+    free(pwm0_dir_name);
+    free(abs_pwm0_dir_name);
+    free(afp_pwm0_run);
+    free(afp_pwm0_period);
+    free(afp_pwm0_duty);
+    free(duty_ns_str1);
+    free(pwm1_dir_name);
+    free(abs_pwm1_dir_name);
+    free(afp_pwm1_run);
+    free(afp_pwm1_period);
+    free(afp_pwm1_duty);
+    free(duty_ns_str2);
+    free(pwm2_dir_name);
+    free(abs_pwm2_dir_name);
+    free(afp_pwm2_run);
+    free(afp_pwm2_period);
+    free(afp_pwm2_duty);
     fprintf(stdout, "===========================================================\n");
     return 0;
 }
