6f34f7b4990808d7724c52a04e742865b13c424b
gobblygobble <gobblygobble@kaist.ac.kr>
2020-06-01 17:04:25 +0900

    lab5 - Get_Key_Var_SDL.cpp

---
+++ lab5/Get_Key_Var_SDL.cpp
@@ -0,0 +1,78 @@
+#include <SDL.h>
+#include <stdio.h>
+
+// screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+// starts up SDL and creates window
+bool init();
+// frees media and shuts down SDL
+void close();
+// the window we'll be rendering to
+SDL_Window* gWindow = NULL;
+// the surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+bool init() {
+	//Initialization flag
+	bool success = true;
+	// init() - 1: SDL_Init()
+	if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else {
+		// init() - 2: SDL_CreateWindow()
+		gWindow = SDL_CreateWindow("EE405 - Get_Key_Var_SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if(gWindow == NULL) {
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else {
+			// init() - 3: SDL_GetWindowSurface()
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+void close() {
+	// close() - 1: SDL_DestroyWindow()
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	// close() - 2: SDL_Quit()
+	SDL_Quit();
+}
+
+int main(int argc, char* args[]) {
+	// 1. init()
+	if(!init()) {
+		printf("Failed to initialize!\n");
+	}
+	else {			
+        // flag to check if main loop will be exited
+        bool quit = false;
+        // event handler
+        SDL_Event e;
+        // main loop... exit only when 'quit' given by user
+        while (!quit) {
+            // handle events on queue
+            while (SDL_PollEvent(&e) != 0) {
+                // user requests quit
+                if(e.type == SDL_QUIT)
+                    quit = true;
+                else {
+                    // print SDL_Scancode, SDL_Keycode, and SDL_Keymod in hexadecimal for each key pressed
+                    printf("Key scancode %xh, keybode %xh, keymod %xh.\n",
+                           e.key.keysym.scancode, e.key.keysym.sym, e.key.keysym.mod);
+                }
+            }
+            // update surface
+            SDL_UpdateWindowSurface(gWindow);
+        }
+    }
+	//Free resources and close SDL
+	close();
+	return 0;
+}
